657c83995f0bf5d6539231560f070bd74cb1fd60
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2021-06-25 01:15:08 -0500

    add put and get skeletons

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const baseRouter = require('./base');
+const productRouter = require('./products');
 
 const router = express.Router();
 
 router.use('/', baseRouter);
+router.use('/products', productRouter);
 
 module.exports = router;
